c5115fe02b35f3e8f1a4ff53fd847471fc4b0cbe
Sreyas-J <Sreyas.Janamanchi@iiitb.ac.in>
2023-08-18 00:18:46 +0530

    basic db querying for chatbot done

--- chatbot/medical_chatbot/models.py
+++ chatbot/medical_chatbot/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class History(models.Model):
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
+
+
+class Prompt(models.Model):
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
+
+
+Sex = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Non-binary', 'Non-binary'),
+)
+
+
+class Patient(models.Model):
+    user = models.ForeignKey(User, related_name='profile', on_delete=models.CASCADE)
+    medical_history = models.ManyToManyField(History, related_name='patient_with_history')
+    prompt_history = models.ManyToManyField(Prompt, related_name='patient_with_prompt')
+    age = models.IntegerField()
+    gender = models.CharField(max_length=15,choices=Sex)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.user.username
--- chatbot/medical_chatbot/views.py
+++ chatbot/medical_chatbot/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import *
+
 from django.http import JsonResponse
 
 import os
@@ -9,25 +11,33 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_KEY", None)
 
-
-from django.http import JsonResponse
-
 def chat(request):
     chatbot_response = None
 
     if openai.api_key is not None and request.method == 'GET':
         user_input = request.GET.get('user_input')
-        prompt = f'Act like a medical assistant or doctor and answer {user_input}'
 
-        response = openai.Completion.create(
-            engine="text-davinci-003",  # You can use "text-davinci-003" for GPT-3.5 Turbo
-            prompt=prompt,
-            max_tokens=200  # Adjust the desired response length
-        )
-        
-        chatbot_response = response.choices[0].text.strip()
+        current_patient = Patient.objects.get(id=1) 
+        medical_history = " * ".join(history.description for history in current_patient.medical_history.all())
+        prompt_history = " * ".join(prompt.description for prompt in current_patient.prompt_history.all())
 
-    print(chatbot_response)  # This will print the chatbot response in your terminal
+        prompt = f'Patient history: {medical_history}\nPatient prompts: {prompt_history}\nAge: {current_patient.age}\nSex: {current_patient.gender}\nHeight: {current_patient.height}\nWeight: {current_patient.weight}\nUser input: {user_input}'
+        print(prompt)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=[
+                {"role": "system", "content": "Try to be a medical assistant and answer the user input."},
+                {"role": "user", "content": prompt}
+            ],
+            #max_tokens=100
+        )   
+        prompt_to_db=Prompt.objects.create(description=user_input) 
+        current_patient.prompt_history.add(prompt_to_db)
+        current_patient.save()
+
+        chatbot_response = response.choices[0].message['content']
 
     return JsonResponse({'response': chatbot_response})
+
+
 
